316cc238032a6a288c119aaddd9892528fb82e15
Megan Thresh <meganthresh96@gmail.com>
2020-05-15 15:17:42 +0100

    pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:index, :show, :new, :create]
+    resources :doses, only: [:new, :create]
   end
-  resources :doses, only: [:destroy] do
-  	resources :ingredients, only: [:index, :show]
-  end
+  resources :doses, only: [:destroy] 
+  resources :ingredients, only: [:index, :show]
 end
